e2cbe4e614061fdc95826f68c1aaf206a533c9a3
rahul more <rahulmore@rahuls-MacBook-Air.local>
2023-12-28 12:32:04 +0530

    fix:toast import

--- utils/validate.js
+++ utils/validate.js
@@ -1,50 +1,43 @@
+import { toast } from "react-toastify";
 export const validateName = (input) => {
-    const nameRegex = /^[A-Za-z\s]+$/;
-    return nameRegex.test(input);
-  };
-
+  const nameRegex = /^[A-Za-z\s]+$/;
+  return nameRegex.test(input);
+};
 
 export const validateEmail = (input) => {
-    // Standard email validation using regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(input);
-  };
-  
+  // Standard email validation using regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(input);
+};
 
- export  const validatePassword = (input) => {
-    const conditions = [
-      { regex: /(?=.*[A-Z])/, message: "at least one uppercase letter" },
-      { regex: /(?=.*[a-z])/, message: "at least one lowercase letter" },
-      { regex: /(?=.*\d)/, message: "at least one digit" },
-      {
-        regex: /(?=.*\W)/,
-        message: "at least one non-alphanumeric character",
-      },
-      {
-        regex: /(?=.*[A-Z][a-z])/,
-        message: "at least one alphabet in uppercase",
-      },
-    ];
-   
+export const validatePassword = (input) => {
+  const conditions = [
+    { regex: /(?=.*[A-Z])/, message: "at least one uppercase letter" },
+    { regex: /(?=.*[a-z])/, message: "at least one lowercase letter" },
+    { regex: /(?=.*\d)/, message: "at least one digit" },
+    {
+      regex: /(?=.*\W)/,
+      message: "at least one non-alphanumeric character",
+    },
+    {
+      regex: /(?=.*[A-Z][a-z])/,
+      message: "at least one alphabet in uppercase",
+    },
+  ];
 
-    for (const condition of conditions) {
-      if (!condition.regex.test(input)) {
-        toast.error(`Password must contain ${condition.message}`);
-        return false;
-      }
-    }
-
-    // Password must be at least 6 characters long (update this message)
-    if (input.length < 6) {
-      toast.error("Password must be at least 6 characters long");
+  for (const condition of conditions) {
+    if (!condition.regex.test(input)) {
+      toast.error(`Password must contain ${condition.message}`);
       return false;
     }
+  }
 
-    // All conditions met
-    return true;
-  };
-  
+  // Password must be at least 6 characters long (update this message)
+  if (input.length < 6) {
+    toast.error("Password must be at least 6 characters long");
+    return false;
+  }
 
-  
- 
-  
+  // All conditions met
+  return true;
+};
